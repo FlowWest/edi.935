f82587793d476bb773fe367125cc9bbe3889ad16
ErinCain <ecain@flowwest.com>
2022-03-24 10:44:27 -0700

    Adds updated data from logan

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -5,7 +5,7 @@ library(lubridate)
 # 7 datasets
 
 # Environmental
-raw_environmental <- read_excel("data-raw/Data/Updated -- 01-10-2022/Environmentals.xlsx")
+raw_environmental <- read_excel("data-raw/Data/Data - Updated 3-9-22/Environmentals.xlsx")
 
 raw_environmental %>%
   mutate(Date = as.Date(Date)) %>%
@@ -18,7 +18,7 @@ write_excel_csv(raw_environmental, "data/Environmentals.csv")
 
 
 # Raw Catch - ByCatch
-raw_bycatch <- read_excel("data-raw/Data/Updated -- 01-10-2022/Raw Catch - ByCatch.xlsx")
+raw_bycatch <- read_excel("data-raw/Data/Data - Updated 3-9-22/Raw Catch - ByCatch.xlsx")
 
 raw_bycatch %>%
   mutate(Date = as.Date(Date)) %>%
@@ -31,7 +31,7 @@ hist(raw_bycatch$n) # All catch describe 1 fish
 write_excel_csv(raw_bycatch, "data/ByCatch.csv")
 
 # Raw Catch - Chinook
-raw_chinook <- read_excel("data-raw/Data/Updated -- 01-10-2022/Raw Catch - Chinook.xlsx", col_types = c("date", "numeric", "text", "numeric",
+raw_chinook <- read_excel("data-raw/Data/Data - Updated 3-9-22/Raw Catch - Chinook.xlsx", col_types = c("date", "numeric", "text", "numeric",
                                                                                                         "numeric", "text", "numeric", "text", "text",
                                                                                                         "text", "text", "numeric", "numeric", "text", "text",
                                                                                                         "text", "text", "numeric", "text", "text"))
@@ -49,7 +49,7 @@ write_excel_csv(raw_chinook, "data/Chinook.csv")
 # Raw Catch - Steelhead
 ## Only 2 steelhead caught
 
-raw_steelhead <- read_excel("data-raw/Data/Updated -- 01-10-2022/Raw Catch - Steelhead.xlsx")
+raw_steelhead <- read_excel("data-raw/Data/Data - Updated 3-9-22/Raw Catch - Steelhead.xlsx")
 raw_steelhead %>%
   mutate(Date = as.Date(Date)) %>%
   glimpse()
@@ -61,7 +61,7 @@ write_excel_csv(raw_steelhead, "data/Steelhead.csv")
 
 # Trap Efficiency Release FL
 
-raw_trap_efficency <- read_excel("data-raw/Data/Updated -- 01-10-2022/Trap Efficiency Release FL.xlsx", col_types = c("numeric", "numeric", "numeric", "text"))
+raw_trap_efficency <- read_excel("data-raw/Data/Data - Updated 3-9-22/Trap Efficiency Release FL.xlsx", col_types = c("numeric", "numeric", "numeric", "text"))
 raw_trap_efficency %>%
   glimpse()
 
@@ -71,7 +71,7 @@ hist(raw_trap_efficency$forkLength)
 write_excel_csv(raw_trap_efficency, "data/TrapEfficiencyRelease.csv")
 
 # Trap Efficiency Summary
-raw_trap_summary <- read_excel("data-raw/Data/Updated -- 01-10-2022/Trap Efficiency Summary.xlsx")
+raw_trap_summary <- read_excel("data-raw/Data/Data - Updated 3-9-22/Trap Efficiency Summary.xlsx")
 raw_trap_summary <-raw_trap_summary %>%
   mutate(includeTestComments = str_replace_all(string = includeTestComments, ",", ""),
          Release.comments = str_replace_all(string = Release.comments, ",", "")) %>%
@@ -82,7 +82,7 @@ hist(raw_trap_summary$nReleased)
 
 write_excel_csv(raw_trap_summary, "data/TrapEfficiencySummary.csv")
 # Trap Operations
-raw_trap_operations <- read_excel("data-raw/Data/Updated -- 01-10-2022/Trap Operations.xlsx")
+raw_trap_operations <- read_excel("data-raw/Data/Data - Updated 3-9-22/Trap Operations.xlsx")
 raw_trap_operations %>%
   mutate(Date = as.Date(Date)) %>%
   glimpse()
